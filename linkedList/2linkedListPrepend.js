1dd5dc254be8114e08e7ae2fefcd9f1327191749
Salman Irfan <salmanirfan674@gmail.com>
2023-08-27 20:17:41 +0500

    linked list prepend, add at the start

---
+++ linkedList/2linkedListPrepend.js
@@ -0,0 +1,38 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+class LinkedList {
+	constructor() {
+		this.head = null;
+		this.size = 0;
+	}
+	// is empty
+	isEmpty() {
+		return this.size === 0;
+	}
+	// getSize
+	getSize() {
+		return this.size;
+	}
+	// prepend
+	prepend(value) {
+		const node = new Node(value);
+		// if linked list is empty
+		if (this.isEmpty()) {
+			this.head = node;
+		} else {
+			node.next = this.head;
+			this.head = node;
+		}
+		this.size++;
+	}
+}
+const list1 = new LinkedList();
+console.log(`isEmpty: ${list1.isEmpty()}`);
+console.log(`getSize: ${list1.getSize()}`);
+list1.prepend(10)
+list1.prepend(20)
+list1.prepend(30)

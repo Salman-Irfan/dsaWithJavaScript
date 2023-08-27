0773b5ce676b07668f5c2391447009a0e3055f43
Salman Irfan <salmanirfan674@gmail.com>
2023-08-27 22:47:38 +0500

    printing the linked list

--- linkedList/2linkedListPrepend.js
+++ linkedList/2linkedListPrepend.js
@@ -29,10 +29,29 @@ class LinkedList {
 		}
 		this.size++;
 	}
+	// print
+	print() {
+		// if linked list is empty
+		if (this.isEmpty()) { 
+			return `linked list is empty`;
+		} else {
+			let curr = this.head
+			let listValues = ``
+			while (curr) {
+				listValues += `${curr.value} `
+				curr=curr.next
+			}
+			console.log(`listValues: ${listValues}`);
+		}
+
+	}
 }
 const list1 = new LinkedList();
 console.log(`isEmpty: ${list1.isEmpty()}`);
 console.log(`getSize: ${list1.getSize()}`);
 list1.prepend(10)
+list1.print()
 list1.prepend(20)
+list1.print()
 list1.prepend(30)
+list1.print()

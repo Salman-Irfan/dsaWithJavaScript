318277e180708c7f96973a410b7c40deb0cf69b3
Salman Irfan <salmanirfan674@gmail.com>
2023-08-28 23:39:17 +0500

    linked list append

--- linkedList/4linkedListAppend.js
+++ linkedList/4linkedListAppend.js
@@ -28,8 +28,22 @@ class LinkedList {
 			this.head = node;
 		}
 		this.size++;
-	}
-	// print
+    }
+    // 4. append
+    append(value) { 
+        const node = new Node(value);
+        if (this.isEmpty()) { 
+            this.head=node;
+        } else {
+            let prev = this.head;
+            while (prev.next) {
+                prev = prev.next;
+            }
+            prev.next = node;
+        }
+        this.size++
+    }
+	// 3. print
 	print() {
 		// if linked list is empty
 		if (this.isEmpty()) {
@@ -49,9 +63,17 @@ const list1 = new LinkedList();
 console.log(`isEmpty: ${list1.isEmpty()}`);
 console.log(`getSize: ${list1.getSize()}`);
 list1.print();
+console.log(`prependMethod`)
 list1.prepend(10);
 list1.print();
 list1.prepend(20);
 list1.print();
 list1.prepend(30);
+list1.print();
+console.log(`appendMethod`)
+list1.append(40);
+list1.print();
+list1.append(50);
+list1.print();
+list1.append(60);
 list1.print();
